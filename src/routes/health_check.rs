17cc5edc6c0eb23994a531d4dd5b26604610e606
Matt Solomon <matt@mattsolomon.dev>
2023-06-09 16:49:22 -0700

    chore: remove logs from health_check

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,10 +1,5 @@
 use axum::http;
-use uuid::Uuid;
 
-#[tracing::instrument(
-    name = "Health check",
-    fields(request_id = %Uuid::new_v4())
-)]
 pub async fn health_check() -> http::StatusCode {
     http::StatusCode::OK
 }
