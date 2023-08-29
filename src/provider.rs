5caefc4fed7949667801979c90b3eb60cef244a3
PraneshASP <praneshas2000@gmail.com>
2023-08-29 19:52:05 +0800

    feat: add avalanche support

--- src/provider.rs
+++ src/provider.rs
@@ -50,6 +50,9 @@ pub fn provider_from_chain(chain: Chain) -> Arc<Provider<Http>> {
         Chain::XDai => {
             Arc::new(Provider::<Http>::try_from(provider_url_from_chain(chain)).unwrap())
         }
+        Chain::Avalanche => {
+            Arc::new(Provider::<Http>::try_from(provider_url_from_chain(chain)).unwrap())
+        }
         _ => panic!("Unsupported chain"),
     }
 }
@@ -65,6 +68,7 @@ pub fn provider_url_from_chain(chain: Chain) -> String {
         Chain::Arbitrum => env::var("ARBITRUM_ONE_RPC_URL").unwrap(),
         Chain::Polygon => env::var("POLYGON_RPC_URL").unwrap(),
         Chain::XDai => env::var("GNOSIS_CHAIN_RPC_URL").unwrap(),
+        Chain::Avalanche => env::var("AVALANCHE_RPC_URL").unwrap(),
         _ => panic!("Unsupported chain"),
     }
 }
@@ -113,6 +117,7 @@ impl MultiChainProvider {
             Chain::Polygon,
             Chain::Sepolia,
             Chain::XDai,
+            Chain::Avalanche,
         ];
 
         let providers = chains
